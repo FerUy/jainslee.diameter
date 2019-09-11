47ab4cbafd9d920bd52665059e28f27ca17d001f
Fernando Mendioroz <fernando.mendioroz@gmail.com>
2019-09-12 01:37:15 +0200

    Fixed MSISDN AVP back to OctetString within User-Identity grouped AVP for Sh RA

--- enablers/hss-client/sbb/src/main/java/org/mobicents/slee/enabler/hssclient/HSSClientChildSbb.java
+++ enablers/hss-client/sbb/src/main/java/org/mobicents/slee/enabler/hssclient/HSSClientChildSbb.java
@@ -478,7 +478,7 @@ public abstract class HSSClientChildSbb implements Sbb, HSSClientChild {
       userIdentityAvp.setPublicIdentity(publicIdentity);
     }
     else if (msisdn != null && msisdn.length > 0) {
-      userIdentityAvp.setMsisdn(new String(msisdn));
+      userIdentityAvp.setMsisdn(msisdn);
     }
 
     return userIdentityAvp;
@@ -501,7 +501,7 @@ public abstract class HSSClientChildSbb implements Sbb, HSSClientChild {
     }
 
     String publicIdentity = null;
-    String msisdn = null;
+    byte[] msisdn = null;
     if(uIdAvp != null) {
       publicIdentity = uIdAvp.getPublicIdentity();
       msisdn = uIdAvp.getMsisdn();
@@ -513,7 +513,7 @@ public abstract class HSSClientChildSbb implements Sbb, HSSClientChild {
       tracer.warning("User-Identity AVP missing in Diameter Sh Message.");
     }
 
-    return new String[]{publicIdentity, msisdn};
+    return new String[]{publicIdentity, String.valueOf(msisdn)};
   }
 
   private ShClientActivity getShClientActivity() throws IOException {
